465ae8572eb5b24d81e74565162623b0777c9bda
Gabriel Martins <gabrielmatheus209@gmail.com>
2021-08-31 16:13:59 -0300

    Estrutura de repeticao ForOf

---
+++ sistemaForOf.js
@@ -0,0 +1,18 @@
+// ForOf no JS
+
+let numeroDeAlunos = 10;
+
+let contador = 0;
+
+let nomesAlunos = ["Rafael", "Handryo", "Gabriel", "Ana"];
+
+for (let nome of nomesAlunos) {
+  console.log(`Esta pessoa se chama ${nome}`);
+}
+
+// ForEach no JS
+
+nomesAlunos.forEach((nome) => {
+  console.log(`O nome é ${nome}`);
+});
+ 
